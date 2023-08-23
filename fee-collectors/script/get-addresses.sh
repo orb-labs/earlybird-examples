d44d8de384dc9bee67c2a8ad39b235dfac522160
Tyler Wanner <tbill29@gmail.com>
2023-08-22 22:25:32 -0400

    some address and get-addresses updates

--- fee-collectors/script/get-addresses.sh
+++ fee-collectors/script/get-addresses.sh
@@ -5,23 +5,17 @@ export ENVIRONMENT=${ENVIRONMENT:-testnet}
 
 chains_directory="environmentVariables/$ENVIRONMENT"
 
-############################################## DEPLOY MOCK APP #############################################################
+############################################## DEPLOY Mock APP #############################################################
 for entry in "$chains_directory"/*
 do
     . "$entry"
+    
     echo "\n\n\n\n############ $CHAIN_NAME ###########"
     
     script_dir=`pwd`
     chain_dir="../addresses/$ENVIRONMENT/$CHAIN_NAME"
+`
     cd $chain_dir
-
-    for e in `ls`; do
-        if [ -d $e ]; then 
-            echo "\n#### $e"
-            for f in `ls $e`; do echo "\n$f -- `cat $e/$f`"; done
-        else
-            echo "\n$e -- `cat $e`"
-        fi
-    done
+    for f in `ls`; do echo "\n$f -- `cat $f`"; done
     cd $script_dir
 done
