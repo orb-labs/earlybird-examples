b004690834f2234a30de1daddd25a5badd83b80d
Richard Adjei <radedoc@yahoo.com>
2024-05-12 00:37:32 -0400

    updating deployment scripts

--- earlybird-ping-pong-app/script/deploy.sh
+++ earlybird-ping-pong-app/script/deploy.sh
@@ -1,27 +1,24 @@
-if [[ -z $GLOBAL_ADDRESSES_DIRECTORY || -z $CHAIN_CONFIGS_DIRECTORY || -z $ENVIRONMENT || -z $KEY_INDEX || -z $MNEMONICS ]]; then echo "env vars unset" && exit 1;fi
+if [[ -z $DEPLOYMENT_CONFIGS_DIRECTORY || -z $KEY_INDEX || -z $MNEMONICS ]]; then echo "env vars unset" && exit 1; fi
 
-# create global address directory
-if [[ ! -d $GLOBAL_ADDRESSES_DIRECTORY ]]; then mkdir $GLOBAL_ADDRESSES_DIRECTORY; fi
-
-# create global address directory for environment
-if [[ ! -d $GLOBAL_ADDRESSES_DIRECTORY_FOR_ENVIRONMENT ]]; then mkdir $GLOBAL_ADDRESSES_DIRECTORY_FOR_ENVIRONMENT; fi
+# create deployment addresses directory
+if [[ ! -d $DEPLOYMENT_ADDRESSES_DIRECTORY ]]; then mkdir $DEPLOYMENT_ADDRESSES_DIRECTORY; fi
 
 # the deploy will run for each file in the chains directory, 
 # all of which should be shell scripts that set env vars specific to the chain
 # the filename should be the chain name
-for entry in "$CHAIN_CONFIGS_DIRECTORY/activeChains"/*
+for entry in "$DEPLOYMENT_CONFIGS_DIRECTORY/activeChains"/*
 do
-    # run script in environmentVariables/ to set env vars for the chain
+    # run script to set env vars for the chain
     . "$entry"
 
-    # create global directory for chain
-    global_address_dir_path_for_chain="$GLOBAL_ADDRESSES_DIRECTORY_FOR_ENVIRONMENT/${CHAIN_NAME}"
-    if [[ ! -d $global_address_dir_path_for_chain ]]; then mkdir $global_address_dir_path_for_chain; fi
+    # create deployment addresses directory for chain
+    deployment_address_dir_for_chain="$DEPLOYMENT_ADDRESSES_DIRECTORY/${CHAIN_NAME}"
+    if [[ ! -d $deployment_address_dir_for_chain ]]; then mkdir $deployment_address_dir_for_chain; fi
     
     # fetch file paths
-    export EARLYBIRD_DATA_FILE_PATH="$global_address_dir_path_for_chain/earlybirdData.txt"
-    export EARLYBIRD_PERIPHERY_CONTRACTS_DATA_FILE_PATH="$global_address_dir_path_for_chain/earlybirdPeripheryContractsData.txt"
-    export EARLYBIRD_PING_PONG_APP_DATA_FILE_PATH="$global_address_dir_path_for_chain/earlybirdPingPongAppData.txt"
+    export EARLYBIRD_DATA_FILE_PATH="$deployment_address_dir_for_chain/earlybirdData.txt"
+    export EARLYBIRD_PERIPHERY_CONTRACTS_DATA_FILE_PATH="$deployment_address_dir_for_chain/earlybirdPeripheryContractsData.txt"
+    export EARLYBIRD_PING_PONG_APP_DATA_FILE_PATH="$deployment_address_dir_for_chain/earlybirdPingPongAppData.txt"
 
     # build and compile contracts
     forge build
--- magiclane-mock-app/script/deploy.sh
+++ magiclane-mock-app/script/deploy.sh
@@ -1,28 +1,25 @@
-if [[ -z $GLOBAL_ADDRESSES_DIRECTORY || -z $CHAIN_CONFIGS_DIRECTORY || -z $ENVIRONMENT || -z $KEY_INDEX || -z $MNEMONICS ]]; then echo "env vars unset" && exit 1;fi
+if [[ -z $DEPLOYMENT_CONFIGS_DIRECTORY || -z $KEY_INDEX || -z $MNEMONICS ]]; then echo "env vars unset" && exit 1; fi
 
-# create global address directory
-if [[ ! -d $GLOBAL_ADDRESSES_DIRECTORY ]]; then mkdir $GLOBAL_ADDRESSES_DIRECTORY; fi
-
-# create global address directory for environment
-if [[ ! -d $GLOBAL_ADDRESSES_DIRECTORY_FOR_ENVIRONMENT ]]; then mkdir $GLOBAL_ADDRESSES_DIRECTORY_FOR_ENVIRONMENT; fi
+# create deployment addresses directory for environment
+if [[ ! -d $DEPLOYMENT_ADDRESSES_DIRECTORY ]]; then mkdir $DEPLOYMENT_ADDRESSES_DIRECTORY; fi
 
 export NUMBER_OF_TOKENS=3;
 
 # the deploy will run for each file in the chains directory, 
 # all of which should be shell scripts that set env vars specific to the chain
 # the filename should be the chain name
-for entry in "$CHAIN_CONFIGS_DIRECTORY/activeChains"/*
+for entry in "$DEPLOYMENT_CONFIGS_DIRECTORY/activeChains"/*
 do
-    # run script in environmentVariables/ to set env vars for the chain
+    # run script to set env vars for the chain
     . "$entry"
 
-    # create global directory for chain
-    global_address_dir_path_for_chain="$GLOBAL_ADDRESSES_DIRECTORY_FOR_ENVIRONMENT/${CHAIN_NAME}"
-    if [[ ! -d $global_address_dir_path_for_chain ]]; then mkdir $global_address_dir_path_for_chain; fi
+    # create deployment addresses directory for chain
+    deployment_addresses_dir_for_chain="$DEPLOYMENT_ADDRESSES_DIRECTORY/${CHAIN_NAME}"
+    if [[ ! -d $deployment_addresses_dir_for_chain ]]; then mkdir $deployment_addresses_dir_for_chain; fi
 
     # fetch file paths
-    export MAGICLANE_MOCK_APP_DATA_FILE_PATH="$global_address_dir_path_for_chain/magiclaneMockApp.txt"
-    export MAGICLANE_SPOKE_DATA_FILE_PATH="$global_address_dir_path_for_chain/magiclaneSpokeData.txt"
+    export MAGICLANE_MOCK_APP_DATA_FILE_PATH="$deployment_addresses_dir_for_chain/magiclaneMockApp.txt"
+    export MAGICLANE_SPOKE_DATA_FILE_PATH="$deployment_addresses_dir_for_chain/magiclaneSpokeData.txt"
 
     # build and compile contracts
     forge build
