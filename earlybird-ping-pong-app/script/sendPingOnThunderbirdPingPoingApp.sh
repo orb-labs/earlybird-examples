4875eecba510463fb2910643ae7b9fe5505c254a
felimadu <fmadutsa@gmail.com>
2024-06-02 20:03:29 -0400

    script for interracting with mock app

--- earlybird-ping-pong-app/script/sendPingOnThunderbirdPingPoingApp.sh
+++ earlybird-ping-pong-app/script/sendPingOnThunderbirdPingPoingApp.sh
@@ -1,41 +1,36 @@
 ############################################################# SETTING ENVIRONMENT VARIABLES ################################################################
 
-# Uncomment for local chains
-export ENVIRONMENT=${ENVIRONMENT:-local}
+# set env vars if unset
+### default environment
+: ${ENVIRONMENT:="local"}
 
-# Uncomment for public testnet
-# export ENVIRONMENT="testnet"
+case $ENVIRONMENT in
+    prod)
+        : ${SENDING_KEY_INDEX:="0"}
+        : ${MNEMONICS:=`gcloud secrets versions access latest --secret=activity-runner-mnemonics`}
+        ;;
+    dev)
+        : ${SENDING_KEY_INDEX:="0"}
+        : ${MNEMONICS:=`gcloud secrets versions access latest --secret=activity-runner-mnemonics`}
+        ;;
+    local)
+        : ${SENDING_KEY_INDEX:="5"}
+        : ${MNEMONICS:="test test test test test test test test test test test junk"}
+        ;;
+    *)
+        echo "invalid environment" && exit 1
+        ;;
+esac
+: ${KEY_INDEX:="0"}
+: ${SENDING_MNEMONICS:="$MNEMONICS"}
 
-# Uncomment for mainnet
-# export ENVIRONMENT="mainnet"
-
-CHAIN_CONFIGS_DIRECTORY="environmentVariables/$ENVIRONMENT"
-
-export MNEMONICS="test test test test test test test test test test test junk"
-export SENDING_KEY_INDEX=5
-
-if [ "$ENVIRONMENT" != "local" ]
-then
-    export MNEMONICS=$(op read "op://Security/MockThunderbirdPingPongApp/Sending_mnemonic_phrase")
-    export SENDING_KEY_INDEX=0
-fi
-
-############################################## HELPER FUNCTIONS ############################################################
-
-address_from_filepath() {
-    existing_address_path=$1
-    if [ -f $existing_address_path ]
-    then
-        address=$(<$existing_address_path)
-    else
-        address="0x0000000000000000000000000000000000000000"
-    fi
-    echo $address
-}
+# export env vars needed by the Solidity scripts
+export ENVIRONMENT KEY_INDEX MNEMONICS SENDING_MNEMONICS SENDING_KEY_INDEX
+export DEPLOYMENT_CONFIGS_DIRECTORY="`pwd`/../../../../deployment-configs/${ENVIRONMENT}"
 
 ############################################## SENDING MESSAGE TO APP ############################################################
 i=0
-for entry in "$search_directory"/*
+for entry in "$DEPLOYMENT_CONFIGS_DIRECTORY/chains/activeChains"/*
 do
     . "$entry"
     chains[$i]=$CHAIN_NAME
@@ -44,14 +39,14 @@ done
 
 echo "\n"
 while true; do
-    echo "Please select the source chain you will like to send this ping from:"
+    echo "Please select the source chain you will like to send this message from:"
     select sourceChain in "${chains[@]}"; do
         [[ -n $sourceChain ]] || { echo "Invalid chain. Please try again." >&2; continue; }
         break # valid choice was made; exit prompt.
     done
 
     echo "\n"
-    echo "Please select the destination chain you will like to send this ping to:"
+    echo "Please select the destination chain you will like to send this message to:"
     select destinationChain in "${chains[@]}"; do
         [[ -n $destinationChain ]] || { echo "Invalid chain. Please try again." >&2; continue; }
         break # valid choice was made; exit prompt.
@@ -61,30 +56,16 @@ while true; do
     echo "Enter the number of pings you will like to send:"
     read PINGS
 
-    destinationChainConfigsPath="$search_directory/"$destinationChain".sh" 
-    destination_thunderbird_ping_pong_app_address_path="../addresses/"$ENVIRONMENT"/"$destinationChain"/thunderbird/app.txt"
-    . "$destinationChainConfigsPath"
+    . "$DEPLOYMENT_CONFIGS_DIRECTORY/chains/activeChains/$sourceChain.sh"
 
-    if [ -f "$destination_thunderbird_ping_pong_app_address_path" ]
-    then
-        destination_thunderbird_ping_pong_app_address=$(<$destination_thunderbird_ping_pong_app_address_path)
-        export RECEIVER_ADDRESS=$destination_rukh_ping_pong_app_address
-        export PINGS=$PINGS
-        export RECEIVER_CHAIN_ID=$CHAIN_ID
-    fi
+    export SOURCE_CHAIN_FILE_PATH="`pwd`/../../../../deployment-addresses/${ENVIRONMENT}/${sourceChain}.json"
+    export DESTINATION_CHAIN_FILE_PATH="`pwd`/../../../../deployment-addresses/${ENVIRONMENT}/${destinationChain}.json"
+    export SOURCE_CHAIN=$sourceChain
+    export DESTINATION_CHAIN=$destinationChain
+    export PINGS=$PINGS
+    export LIBRARY="Thunderbird"
 
-    sourceChainConfigsPath="$search_directory/"$sourceChain".sh" 
-    source_thunderbird_ping_pong_app_address_path="../addresses/"$ENVIRONMENT"/"$sourceChain"/app.txt"
-    . "$sourceChainConfigsPath"
-
-    export THUNDERBIRD_PING_PONG_APP_ADDRESS=`address_from_filepath "../addresses/"$ENVIRONMENT"/"$CHAIN_NAME"/thunderbird/app.txt"`
-
-    echo $PINGS
-    echo $RECEIVER_ADDRESS
-    echo $RECEIVER_CHAIN_ID
-    echo $THUNDERBIRD_PING_PONG_APP_ADDRESS
-    echo $RPC_URL
-
-    forge script deploymentScripts/thunderbird/PingPong.s.sol:ThunderbirdPingPongAppSendPing --rpc-url $RPC_URL --broadcast
+    # send message
+    node sendPingOnMockApp.js
     echo "\n"
 done
